b700440ef610ddce5dffd741c42d6ebe43ee1ed7
Hanan <Hanan.bayazeed56@gmail.com>
2026-03-30 20:13:57 +0300

    implement sockets for creating&updating&deleting posts

--- config/socket.js
+++ config/socket.js
@@ -2,12 +2,12 @@ import { Server } from "socket.io";
 import corsOptions from "./corsOptions.js";
 
 let io;
-
+//to create io
 export const init = (httpServer) => {
   io = new Server(httpServer, { cors: corsOptions });
   return io;
 };
-
+//to access our io from other components
 export const getIO = () => {
   if (!io) {
     throw new Error("Socket.io not initialized!");
